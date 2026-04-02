74c98f650faf4da47f56ef639015ee7a5b3cdcfb
neshanpramuditha <neshanpppramuditha@gmail.com>
2026-04-02 15:51:17 +0530

    Update

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -104,9 +104,9 @@ const ChatBot: React.FC = () => {
           </div>
 
           {/* Input Area */}
-          <div className="p-4 bg-white border-t border-gray-100 flex gap-2">
+          <div className="p-4 bg-white border-t border-gray-100 flex gap-2 text-black">
             <input 
-              className="flex-1 bg-gray-100 border-none rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500 outline-none"
+              className="flex-1 bg-gray-100 border-none rounded-lg px-3 py-2 text-sm focus:ring-2 focus:primary/80 outline-none"
               placeholder="Ask me anything..."
               value={input}
               onChange={(e) => setInput(e.target.value)}
